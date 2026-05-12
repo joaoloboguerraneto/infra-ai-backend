918603930329feae5025ff0f91a9b74aeb2e886b
Joao Lobo <joao-lobo@e-unicred.com.br>
2026-05-12 15:02:40 -0300

    fix: populate templates __init__.py with get_registry

---
+++ app/templates/__init__.py
@@ -0,0 +1,15 @@
+from .base import TerraformTemplate
+from .s3 import S3BucketTemplate
+from .lambda_ import LambdaFunctionTemplate
+from .sqs import SQSQueueTemplate
+
+
+def get_registry() -> dict:
+    """
+    Retorna {name -> instancia} de todos os templates.
+    Adicionar recurso = criar arquivo + importar aqui.
+    """
+    return {cls.name: cls() for cls in TerraformTemplate.__subclasses__()}
+
+
+__all__ = ["get_registry", "TerraformTemplate"]
